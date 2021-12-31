be85ec9a59afd7297c76a813fcff9079e336e50c
Tomas P <madness.hasek@seznam.cz>
2021-12-31 17:55:31 +0100

    game loop

---
+++ app/src/main/java/com/example/tank1990/Game.java
@@ -0,0 +1,62 @@
+package com.example.tank1990;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import androidx.annotation.NonNull;
+
+public class Game extends SurfaceView implements SurfaceHolder.Callback {
+
+    private GameLoop gameLoop;
+    private Performance performance;
+
+    public Game(Context context) {
+        super(context);
+
+        SurfaceHolder surfaceHolder = getHolder();
+        surfaceHolder.addCallback(this);
+
+        gameLoop = new GameLoop(this, surfaceHolder);
+        performance = new Performance(gameLoop, context);
+
+        setFocusable(true);
+    }
+
+
+    @Override
+    public void surfaceCreated(@NonNull SurfaceHolder surfaceHolder) {
+        if (gameLoop.getState().equals(Thread.State.TERMINATED)) {
+            gameLoop = new GameLoop(this, surfaceHolder);
+        }
+
+        gameLoop.startLoop();
+    }
+
+    @Override
+    public void surfaceChanged(@NonNull SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(@NonNull SurfaceHolder surfaceHolder) {
+
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        super.draw(canvas);
+
+        performance.draw(canvas);
+    }
+
+    public void update() {
+
+
+    }
+
+    public void pause() {
+        gameLoop.stopLoop();
+    }
+}
---
+++ app/src/main/java/com/example/tank1990/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.tank1990;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class MainActivity extends Activity {
+
+    private Game game;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        game = new Game(this);
+        setContentView(game);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        game.pause();
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onBackPressed() {
+    }
+}
